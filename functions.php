bfd37fb8c66ee80ea6d0a06d516457871b24a8ab
KEVIN RAGSDALE <118561174+Kevin6757@users.noreply.github.com>
2022-12-09 19:39:43 +0000

    Added the stuff to template because we accidently edited main hehe

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+$mainNav = '
+<li class="nav-item"><a class="nav-link active" aria-current="page" href="/index.php">Home</a></li>
+<li class="nav-item"><a class="nav-link active" href="/form.php">Form</a></li>
+<li class="nav-item"><a class="nav-link active" href="/friends.php">Friends</a></li>
+<li class="nav-item"><a class="nav-link active" href="/family.php">Family</a></li>
+<li class="nav-item"><a class="nav-link active" href="/coworkers.php">Co-Workers</a></li>
+ 
+ 
+  ';
+ 
+ 
+?>
