cd34df789824be7d2565bd04c6ea98313406e625
BRANDON STECHER <119446646+Bstech06@users.noreply.github.com>
2023-01-09 19:00:22 +0000

    made data have uid

--- test.php
+++ test.php
@@ -1,29 +1,19 @@
 
 <?php
-$data = file_get_contents($_SERVER['DOCUMENT_ROOT']. "/data.json");
-  $data = json_decode($data, TRUE);
+$data = file_get_contents($_SERVER['DOCUMENT_ROOT']."/data.json");      
+  $data =  json_decode($data, TRUE);
 
-
-$uid = 101;
-  foreach ($data as &$item){
-    $item['uid'] = $uid;
-    $uid++;
- 
- 
- 
-   
- 
-   
+  $uid = 201;
+  foreach ($data as &$item) {
+      $item['uid'] = $uid;
+      $uid++;
   }
-$data = json_encode($data, TRUE);
-$filename  = $_SERVER['DOCUMENT_ROOT']. "/data.json";
-file_put_contents($filename, $data);
 
-
+$data =  json_encode($data);
 $filename = $_SERVER['DOCUMENT_ROOT']."/data.json";
 file_put_contents($filename, $data);
-
-// echo '<pre>';
-// var_dump($data);
-// echo '</pre>';
+ 
+  echo '<pre>';
+  var_dump($data);
+  echo '</pre>';
 ?>
